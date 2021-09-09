02e71c7df86401bd0978505e6452fca02582c355
tnakamura0314 <Reggae.a0215@gmail.com>
2021-09-09 13:47:07 +0900

    CSVファイルを読み込み、コンソールに出力する処理２パターンの作成

--- src/main/java/com/example/CsvReadStream.java
+++ src/main/java/com/example/CsvReadStream.java
@@ -1,5 +1,39 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 public class CsvReadStream {
+	
+	public static void main(String[] args) {
+        // BufferedReader.readLineでCSVファイルの行を1行ずつ読み込む
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                new FileInputStream("sample.csv"), Charset.forName("utf-8")))) {
+            String line;
+            int index = 0;
+            while ((line = reader.readLine()) != null) {
+                if (index > 0) {
+                	
+                	//Stringの「split」メソッドでカンマ区切りの文字列を分割 → Stringの配列に代入
+                    String[] data = line.split(",");
+                    if (data.length > 3) {
+                        // 読み込んだCSVファイルの内容を出力
+                        System.out.print(data[0] + ",");
+                        System.out.print(data[1] + ",");
+                        System.out.print(data[2] + ",");
+                        System.out.print(data[3] + ",");
+                        System.out.print(data[4] + ",");
+                        System.out.println(data[5]);
+                    }
+                }
+                index++;
+            }
+        } catch (IOException e) {
+            System.out.println("ファイル読み込みに失敗");
+        }
+    }
 
 }
