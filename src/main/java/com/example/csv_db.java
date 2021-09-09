997e8c233258903be8f225dece21360c687a2e63
tnakamura0314 <Reggae.a0215@gmail.com>
2021-09-09 19:13:39 +0900

    DBの情報からcsvを出力/csvからDBにinsertの修正

--- src/main/java/com/example/csv_db.java
+++ src/main/java/com/example/csv_db.java
@@ -8,36 +8,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-public class jdbc {
-	
+public class csv_db {
+
 	public static void main(String[] args) {
 		try {
 			// インストールしたpostgreSQLのドライバを指定
 			Class.forName("org.postgresql.Driver");
 
 			// postgreSQLデータベースに接続 (DB名,ID,パスワードを指定)
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ec-202107c", "postgres", "postgres");
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ec-202107c", "postgres",
+					"postgres");
 
 			// ステートメントを作成
-			PreparedStatement stmts = conn.prepareStatement("INSERT INTO items(id,name,description,price_m,price_l,image_path,deleted) VALUES (?, ?, ?, ?, ?, ?, ?);");
+			PreparedStatement stmts = conn.prepareStatement(
+					"INSERT INTO items(id,name,description,price_m,price_l,image_path,deleted) VALUES (?, ?, ?, ?, ?, ?, ?);");
 
 			try {
 				File file = new File("sample.csv");
 				BufferedReader br = new BufferedReader(new FileReader(file));
+				
+//				System.out.println(br.readLine());
 
 				String line;
 				// 1行ずつCSVファイルを読み込む
 				while ((line = br.readLine()) != null) {
 
-					String[] data = line.split(","); // 行をカンマ区切りで配列に変換
-					System.out.println(data[0]);
-					System.out.println(data[1]);
-					System.out.println(data[2]);
-					System.out.println(data[3]);
-					System.out.println(data[4]);
-					System.out.println(data[5]);
-					System.out.println(data[6]);
+					//csvのカラムの1行をスキップ
+					if (line.equals("id,name,description,price_m,price_l,image_path,deleted")) {
+						continue;
+					}
 
+					String[] data = line.split(","); // 行をカンマ区切りで配列に変換
+
+//					System.out.println(data[0]);
+//					System.out.println(data[1]);
+//					System.out.println(data[2]);
+//					System.out.println(data[3]);
+//					System.out.println(data[4]);
+//					System.out.println(data[5]);
+//					System.out.println(data[6]);
 
 					stmts.setInt(1, Integer.parseInt(data[0]));
 					stmts.setString(2, data[1]);
@@ -47,7 +56,8 @@ public class jdbc {
 					stmts.setString(6, data[5]);
 					stmts.setBoolean(7, Boolean.valueOf(data[6]));
 					stmts.executeUpdate();
-
+					
+					System.out.println("インサート完了！！");
 
 				}
 				br.close();
---
+++ src/main/java/com/example/db_csv.java
@@ -0,0 +1,106 @@
+package com.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class db_csv {
+
+	// カンマ
+	private static final String COMMA = ",";
+	// 改行
+	private static final String NEW_LINE = "\n";
+
+	public static void main(String[] args) {
+
+		try {
+			// インストールしたpostgreSQLのドライバを指定
+			Class.forName("org.postgresql.Driver");
+
+			// postgreSQLデータベースに接続 (DB名,ID,パスワードを指定)
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ec-202107c", "postgres",
+					"postgres");
+
+			// ステートメントを作成
+			PreparedStatement stmts = conn
+					.prepareStatement("SELECT id,name,description, price_m, price_l, image_path, deleted FROM items ;");
+
+			ResultSet rs = stmts.executeQuery();
+
+			// Itemクラスの空のListを作成
+			List<Item> itemList = new ArrayList<>();
+
+			while (rs.next()) {
+				Item item = new Item();
+				item.setId(rs.getInt("id"));
+				item.setName(rs.getString("name"));
+				item.setDescription(rs.getString("description"));
+				item.setPriceM(rs.getInt("price_m"));
+				item.setPriceL(rs.getInt("price_l"));
+				item.setImagePath(rs.getString("image_path"));
+				item.setDeleted(rs.getBoolean("deleted"));
+				System.out.println(item);
+				itemList.add(item);
+			}
+			System.out.println(itemList);
+
+			FileWriter fileWriter = null;
+
+			try {
+				fileWriter = new FileWriter("item.csv");
+				
+				fileWriter.append("id");
+				fileWriter.append(COMMA);
+				fileWriter.append("name");
+				fileWriter.append(COMMA);
+				fileWriter.append("price_m");
+				fileWriter.append(COMMA);
+				fileWriter.append("price_l");
+				fileWriter.append(COMMA);
+				fileWriter.append("image_path");
+				fileWriter.append(COMMA);
+				fileWriter.append("deleted");
+				fileWriter.append(NEW_LINE);
+				
+
+				// リストの内容を順に処理
+				for (Item item : itemList) {
+
+					fileWriter.append(String.valueOf(item.getId()));
+					fileWriter.append(COMMA);
+					fileWriter.append(item.getName());
+					fileWriter.append(COMMA);
+					fileWriter.append(String.valueOf(item.getPriceM()));
+					fileWriter.append(COMMA);
+					fileWriter.append(String.valueOf(item.getPriceL()));
+					fileWriter.append(COMMA);
+					fileWriter.append(item.getImagePath());
+					fileWriter.append(COMMA);
+					fileWriter.append(item.getDescription());
+					fileWriter.append(NEW_LINE);
+				}
+
+				System.out.println("CSVファイル出力完了");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+
+				try {
+					fileWriter.flush();
+					fileWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
